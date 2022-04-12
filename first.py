0030e4eb7661fb78783f55707ec50e51f74d6064
nitwit-3 <103535178+nitwit-3@users.noreply.github.com>
2022-04-12 16:18:33 -0400

    Create first.py

---
+++ first.py
@@ -0,0 +1,15 @@
+#! /bin/python3
+# Print String
+import sys
+
+print("Hello World!") # double quote
+ 
+print('\n') # print new line
+
+print('Hello World') # single quote
+
+print("""This string runs 
+multiple lines!""") # triple quote for single line 
+
+print("This string is "+" awesome") # we can also concationate 
+sys.exit()
